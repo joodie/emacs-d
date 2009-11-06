bc96834fdfc1b68b31da9c57182c5fbc43b58182
Joost Diepenmaat <joost@zeekat.nl>
2009-11-06 14:16:23 +0100

    ido customizations

--- custom.el
+++ custom.el
@@ -3,7 +3,14 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- )
+ '(ido-enable-flex-matching t)
+ '(ido-enable-regexp t)
+ '(ido-everywhere t)
+ '(ido-max-file-prompt-width 0)
+ '(ido-max-prospects 0)
+ '(ido-max-window-height 30)
+ '(ido-use-filename-at-point (quote guess))
+ '(ido-use-url-at-point nil))
 (custom-set-faces
   ;; custom-set-faces was added by Custom.
   ;; If you edit it by hand, you could mess it up, so be careful.
