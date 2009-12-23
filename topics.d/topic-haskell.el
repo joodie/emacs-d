dc9782416155414f73ba8ea420b8592ca4e3fc1b
Joost Diepenmaat <joost@zeekat.nl>
2009-12-23 15:54:18 +0100

    added haskell-mode

---
+++ topics.d/topic-haskell.el
@@ -0,0 +1,5 @@
+(load "~/.emacs.d/ext/haskell-mode/haskell-site-file.el")
+(add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
+(add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
+
+(provide 'topic-haskell)
