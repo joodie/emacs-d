75a10a8f0c3d5a824c9b8299ce71c24ab147c7da
Joost Diepenmaat <joost@zeekat.nl>
2010-05-27 11:59:26 +0200

    ruby stuff added

---
+++ topics.d/topic-ruby.el
@@ -0,0 +1,19 @@
+(add-ext-load-path "emacs-rails")
+(require 'ruby-mode)
+
+(setq auto-mode-alist
+      (append '(("\\.rb$" . ruby-mode)) auto-mode-alist))
+(setq interpreter-mode-alist (append '(("ruby" . ruby-mode))
+                                     interpreter-mode-alist))
+
+(autoload 'run-ruby "inf-ruby"
+  "Run an inferior Ruby process")
+(autoload 'inf-ruby-keys "inf-ruby"
+  "Set local key defs for inf-ruby in ruby-mode")
+(add-hook 'ruby-mode-hook
+          '(lambda ()
+             (inf-ruby-keys)
+    ))
+
+(require 'rails)
+(provide 'topic-ruby)
