c0c6633d95e6d5305e103473ef727c6872dbd6d7
Joost Diepenmaat <joost@zeekat.nl>
2010-06-03 23:58:30 +0200

    use text menus in rails mode

--- custom.el
+++ custom.el
@@ -3,9 +3,16 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
+ '(android-mode-sdk-dir "~/lib/android-sdk")
  '(blink-cursor-mode t)
  '(column-number-mode t)
  '(display-time-mode t)
- '(android-mode-sdk-dir "~/lib/android-sdk")
+ '(rails-always-use-text-menus t)
  '(smart-tab-completion-functions-alist (quote ((emacs-lisp-mode . lisp-complete-symbol) (text-mode . dabbrev-completion) (clojure-mode . slime-complete-symbol))))
  '(swank-clojure-binary "/usr/bin/clj"))
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ )
