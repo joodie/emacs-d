51d393e45d4c5ead3d7512f623026f8a79c8afd0
Joost Diepenmaat <joost@zeekat.nl>
2010-08-11 22:08:56 +0200

    update clojure stuff

--- topics.d/topic-clojure.el
+++ topics.d/topic-clojure.el
@@ -25,14 +25,40 @@
 
 (setq slime-complete-symbol-function 'slime-complete-symbol*)
 
-(defun clojure (binary)
-  (interactive "fbinary: ")
-  (setq swank-clojure-binary (or binary "clj"))
-  (setq slime-lisp-implementations
-        `((clojure (,swank-clojure-binary) :init swank-clojure-init))) 
-  (slime))
+;; based on http://github.com/purcell/emacs.d/blob/master/init-clojure.el
+
+(defun slime-clojure-repl-setup ()
+  (when (string-equal "clojure" (slime-connection-name))
+    (message "Setting up repl for clojure")
+    (when (slime-inferior-process)
+      (slime-redirect-inferior-output))
+    (paredit-mode 1)
+    (set-syntax-table clojure-mode-syntax-table)
+    (clojure-mode-font-lock-setup)
+
+    (setq lisp-indent-function 'clojure-indent-function)
+
+    (when (and (featurep 'paredit) paredit-mode (>= paredit-version 21))
+      (define-key slime-repl-mode-map "{" 'paredit-open-curly)
+      (define-key slime-repl-mode-map "}" 'paredit-close-curly))))
+
+(add-hook 'slime-repl-mode-hook 'slime-clojure-repl-setup)
 
 
 
+(defun lein-swank ()
+  (interactive)
+  (let ((root (locate-dominating-file default-directory "project.clj")))
+    (when (not root)
+      (error "Not in a Leiningen project."))
+    ;; you can customize slime-port using .dir-locals.el
+    (shell-command (format "cd %s && lein swank %s &" root slime-port)
+                   "*lein-swank*")
+    (set-process-filter (get-buffer-process "*lein-swank*")
+                        (lambda (process output)
+                          (when (string-match "Connection opened on" output)
+                            (slime-connect "localhost" slime-port)
+                            (set-process-filter process nil))))
+    (message "Starting swank server...")))
 
 (provide 'topic-clojure)
